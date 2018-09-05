e34d6fe9b552145e9f657f60b680e328a4d63695
Vu Dao <mr.minhvu.dao@gmail.com>
2018-09-05 08:30:53 +0700

    Update oxfordDict.py

--- oxford/oxfordDict.py
+++ oxford/oxfordDict.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+'''
+    Search Dictionaries
+    www.oxfordlearnersdictionaries.com/
+    REST API documentation: https://www.oxfordlearnersdictionaries.com/api/v1/documentation/html
+    Register for app_key: https://developer.oxforddictionaries.com/
+'''
 import requests
 import json
 import argparse
@@ -22,16 +28,46 @@ class OxfordDict():
         self.language = 'en'
         self.url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + self.language + '/' \
                           + self.word.lower()
+        self.count_def = 1
+
+    def chunk_str(self, str_def):
+        ''' Split the string by 2 for nice print '''
+        if len(str_def) > 80:
+            str_split = str_def.split()
+            chunk, chunk_size = len(str_split), len(str_split) / 2
+            str_def = "\n".join([" ".join(str_split[i:i+chunk_size]) for i in range(0, chunk, chunk_size)])
+        return str_def
 
     def display(self):
+        '''
+        Display the main definition of word and examples.
+        Then display the subsenses and examples
+        '''
         r = requests.get(self.url, headers={'app_id': self.app_id, 'app_key': self.app_key})
         data = json.loads(json.dumps(r.json()))
-        str = data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
-        str_split = str.split()
-        chunck, chunck_size = len(str_split), len(str_split)/3
-        print (bcolors.BOLD + bcolors.YELLOW + self.word + bcolors.ENDC)
-        print "\n".join([" ".join(str_split[i:i+chunck_size]) for i in range(0, chunck, chunck_size)])
+        definition = data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0]
+        examples = data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['examples']
+        print ("  " + bcolors.BOLD + bcolors.YELLOW + self.word + bcolors.ENDC)
+        print (str(self.count_def) + ". " + self.chunk_str(definition))
+        self.count_def += 1
+        for i in range(0, len(examples), 1):
+            print (bcolors.BOLD + bcolors.RED + "  . " + bcolors.ENDC + examples[i]['text'])
 
+        subsenses = data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['subsenses']
+        self.display_subsenses(subsenses)
+
+    def display_subsenses(self, str_sub):
+        '''
+        Loop all the subsenses and their examples
+        '''
+        for i in range(0, len(str_sub), 1):
+            _def = str_sub[i]['definitions'][0]
+            print str(self.count_def) + ". " + _def
+            self.count_def += 1
+            if 'examples' in str_sub[i]:
+                _example = str_sub[i]['examples']
+                for i in range(0, len(_example), 1):
+                    print (bcolors.BOLD + bcolors.RED + "  . " + bcolors.ENDC + _example[i]['text'])
 
 def parse_argument(args):
     parser = argparse.ArgumentParser(description="Oxford learner's dictionaries",
