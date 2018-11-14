bae95fae91d82d9776151d05f9e98c6b1c271416
Vu Dao <mr.minhvu.dao@gmail.com>
2018-11-14 08:20:00 +0700

    Update test_immObject using 'with' statement
    - 'with' statement to try __enter__, __exit__, and __del__
    - Upload pybook

--- opensaf/immObject/test_immObject.py
+++ opensaf/immObject/test_immObject.py
@@ -14,33 +14,37 @@ class CreateObject:
             self.parent = self.dn.split(',')[1]
         else:
             self.parent = None
-        self.obj = None
 
-    def init_obj(self):
         self.obj = ImmObject(class_name=self.class_name, dn=self.dn)
-        import pdb;pdb.set_trace()
         for attr in self.attributes.keys():
-            self.obj.attrs[attr] = (self.obj.get_value_type(attr), [self.attributes[attr]]) 
+            self.obj.attrs[attr] = (self.obj.get_value_type(attr), [self.attributes[attr]])
 
     def init_ccb(self):
         ccb = Ccb(flags=None)
         ccb.init()
+        return ccb
 
+    def ccb_create(self):
+        ccb = self.init_ccb()
         ccb.create(self.obj, parent_name=self.parent)
         ccb.apply()
         ccb.finalize()
-    
-    def main(self):
-        self.init_obj()
-        self.init_ccb()
+
+    def ccb_mod(self, attr, values):
+        """ Try the 'with' statement """
+        with Ccb(flags=None) as ccb:
+            ccb.init()
+            ccb.modify_value_add(self.dn, attr, values)
+            #ccb.apply()
 
 if __name__ == "__main__":
     c_attr_dict = {'attribute1': 'a1', 'attribute2': 'a2', 'sampleClassId': 'sampleClassId=1'}
     c_dn = 'sampleClassId=1'
     c = CreateObject("SampleClass1", c_attr_dict, c_dn)
-    c.main()
+    c.ccb_create()
+    c.ccb_mod('attribute1', 'sam1')
 
     d_attr_dict = {'lowerCaps': 'low', 'upperCaps': 'up', 'capsId': 'capsId=1'}
     d_dn = 'capsId=1,sampleClassId=1'
     d = CreateObject("CapsSample", d_attr_dict, d_dn)
-    d.main()
+    d.ccb_create()
