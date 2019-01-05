ddf904d821b43dbd866ba5727223c11c15367082
Vu Dao <mr.minhvu.dao@gmail.com>
2019-01-05 09:55:30 +0700

    Add new features to oxfordDict.py
    - Get more subsenses and word types
    - Show count type of noun
    - Add option '--type' to filter type of word

--- oxford/README.md
+++ oxford/README.md
@@ -9,3 +9,5 @@
   chmod +x oxfordDict.py
   cp oxfordDict.py /usr/local/bin/oxfordDict
 
+- Raw command:
+  curl -X GET -H "Accept: application/json" https://od-api.oxforddictionaries.com:443/api/v1/entries/en/light -H 'app_id: 79b0bf7f' -H 'app_key: e1c41553a15e7ab359b36ec3a849e2b4'
--- oxford/oxfordDict.py
+++ oxford/oxfordDict.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 '''
     Search Dictionaries
-    www.oxfordlearnersdictionaries.com/
+    www.oxfordlearnersdictionaries.com/ or https://en.oxforddictionaries.com/
     REST API documentation: https://www.oxfordlearnersdictionaries.com/api/v1/documentation/html
     Register for app_key: https://developer.oxforddictionaries.com/
 '''
@@ -9,6 +9,7 @@ import requests
 import json
 import argparse
 import sys
+import re
 
 class bcolors:
     HEADER = '\033[95m'
@@ -29,69 +30,114 @@ class OxfordDict():
         self.url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + self.language + '/' \
                           + self.word.lower()
         self.count_def = 1
-        self.type_dict = {"Noun": "(n)", "Verb": "(v)", "Adjective": "(a)"}
+        self.word_type_dict = {"Noun": "n", "Verb": "v", "Adjective": "adj", "Adverb": "adv"}
 
-    def display(self):
-        '''
-        Display the main definition of word and examples.
-        Then display the subsenses and examples
-        '''
+    def build_data(self):
+        """
+        Build data: {word type: [senses, pron]}
+        - Each item of 'lexicalEntries' is a word type that is defined in 'lexicalCategory'
+          and it contains definitions and examples
+        """
         r = requests.get(self.url, headers={'app_id': self.app_id, 'app_key': self.app_key})
         if r.status_code == 404:
             print(bcolors.BOLD + "\"" + self.word + "\"" + bcolors.ENDC + " Not Found")
-            exit(0)
-        data = json.loads(json.dumps(r.json()))
-        senses = data['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]
-        type = data['results'][0]['lexicalEntries'][0]['lexicalCategory']
-        pron = data['results'][0]['lexicalEntries'][0]['pronunciations'][0]['phoneticSpelling']
+            sys.exit(0)
+        json_dump = json.loads(json.dumps(r.json()))
+        data = {}
+        for lex_dict in json_dump['results'][0]['lexicalEntries']:
+            word_type = lex_dict['lexicalCategory']
+            senses = lex_dict['entries'][0]['senses'][0]
+            pron = lex_dict['pronunciations'][0]['phoneticSpelling']
+            data[word_type] = [senses, pron]
+        return data
 
-        print (bcolors.BOLD + bcolors.YELLOW + " {} /".format(self.word) + pron + "/ "
-               + self.type_dict[type] + bcolors.ENDC)
-        print (str(self.count_def) + ". " + self.chunk_str(senses['definitions'][0]))
-        self.count_def += 1
+    def display(self, type_get):
+        """
+        Display the main definition of word and examples.
+        Then display the subsenses and examples
+        """
 
-        if 'examples' in senses:
-            self.display_examples(senses['examples'])
+        def chunk_str(str_def):
+            """ Split the string by 2 for nice print """
+            if len(str_def) > 80:
+                str_split = str_def.split()
+                chunk, chunk_size = len(str_split), len(str_split) / 2
+                str_def = "\n".join([" ".join(str_split[i:i + chunk_size]) for i in range(0, chunk, int(chunk_size))])
+            return str_def
 
-        if 'subsenses' in senses:
-            self.display_subsenses(senses['subsenses'])
+        def display_examples(examples):
+            for e in examples:
+                print(bcolors.BOLD + bcolors.RED + "%3s %s" % (".", e['text']) + bcolors.ENDC)
+            print("\r")
 
-    def display_subsenses(self, str_sub):
-        '''
-        Loop all the subsenses and their examples
-        '''
-        for i in range(0, len(str_sub), 1):
-            _def = str_sub[i]['definitions'][0]
-            print str(self.count_def) + ". " + _def
+        def display_subsenses(subsenses, order):
+            """ Loop all the subsenses and their examples """
+            count = 1
+            sub_note_text = ""
+            for subsense in subsenses:
+                if 'notes' in subsense:
+                    tmp_note = subsense['notes'][0]['text']
+                    if tmp_note.find('"') != -1:
+                        tmp_note = re.sub('"', "", tmp_note)
+                        sub_note_text = "({})".format(tmp_note)
+                    else:
+                        sub_note_text = "[{}]".format(tmp_note)
+                _def = subsense['definitions'][0]
+                item = "{}.{} {}".format(order, count, sub_note_text)
+                print("%5s %s" % (item, _def))
+                count += 1
+                if 'examples' in subsense:
+                    display_examples(subsense['examples'])
+
+        data = self.build_data()
+        for word_type, content in data.items():
+            if type_get:
+                if self.word_type_dict[word_type] != type_get:
+                    continue
+
+            note_text = ""
+            pron = content[1]
+            sense = content[0]
+            if 'notes' in sense:
+                note_text = "[{}]".format(sense['notes'][0]['text'])
+
+            print(bcolors.BOLD + bcolors.YELLOW + "%s /%s/ (%s)" % (self.word, pron, self.word_type_dict[word_type]) \
+                  + bcolors.ENDC)
+            print("{0}. {1} {2}".format(str(self.count_def), note_text, chunk_str(sense['definitions'][0])))
+
+            if 'examples' in sense:
+                display_examples(sense['examples'])
+
+            if 'subsenses' in sense:
+                display_subsenses(sense['subsenses'], self.count_def)
+
             self.count_def += 1
-            if 'examples' in str_sub[i]:
-                self.display_examples(str_sub[i]['examples'])
 
-    def chunk_str(self, str_def):
-        ''' Split the string by 2 for nice print '''
-        if len(str_def) > 80:
-            str_split = str_def.split()
-            chunk, chunk_size = len(str_split), len(str_split) / 2
-            str_def = "\n".join([" ".join(str_split[i:i+chunk_size]) for i in range(0, chunk, chunk_size)])
-        return str_def
-
-    def display_examples(self, examples):
-        for i in range(0, len(examples), 1):
-            print (bcolors.BOLD + bcolors.RED + "  . " + bcolors.ENDC + examples[i]['text'])
 
 def parse_argument(args):
     parser = argparse.ArgumentParser(description="Oxford learner's dictionaries",
                                      epilog="Search OALD")
     parser.add_argument("input", type=str, nargs=1,
                         help="Input word to search")
+    parser.add_argument("-t", "--type", type=str, nargs=1, dest="word_type",
+                        help="Type of word: n|v|adj|adv")
 
     options = parser.parse_args(args=args)
     return options
 
+
 def main():
     options = parse_argument(sys.argv[1:])
     oxf = OxfordDict(options.input[0])
-    oxf.display()
+
+    word_type = ""
+    if options.word_type:
+        word_type = options.word_type[0]
+        if word_type not in oxf.word_type_dict.values():
+            print("No Type of word\n\nType of word: n|v|adj|adv")
+            sys.exit(1)
+    oxf.display(word_type)
+
 
 if __name__ == "__main__":
     main()
